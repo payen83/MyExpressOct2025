bc2cc56f18ce76d8da8c3a83adca83d80a694812
Ferdy Fauzi <payen83@gmail.com>
2025-10-30 10:02:00 +0800

    update db

--- database.js
+++ database.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 const database = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'FErd1983~!',
-    database: 'mymahirdb'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 (async() => {
